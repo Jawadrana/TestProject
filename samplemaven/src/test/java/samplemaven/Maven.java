ac54d6ff1e59cc7b7251e0e7490a38d6084fa547
Home <Home@DESKTOP-OUL848Q>
2019-08-23 00:39:49 -0400

    First commit

---
+++ samplemaven/src/test/java/samplemaven/Maven.java
@@ -0,0 +1,119 @@
+package samplemaven;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Maven {
+	String baseUrl = "https://www.demo.iscripts.com/socialware/demo//";
+	public WebDriver driver = null;
+
+	@BeforeMethod
+	public void Setup(){
+
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(20,TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		
+		
+	}
+	
+	@BeforeTest
+	public void LaunchBrowser() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get(baseUrl);
+		System.out.println("Browser Opened");
+
+	}
+
+	@Test(priority=0)
+	public void VerifyHomepageTitle() {
+		String expectedTitle = "Socialware";
+		String actualTitle= driver.getTitle();
+		Assert.assertEquals(actualTitle, expectedTitle);
+	}
+
+	@Test(priority=1)
+	public void UserRegistration() {
+		driver.findElement(By.xpath("//*[@id=\"user_emails\"]")).sendKeys("user@22iscripts.com");
+		System.out.println("Email Entered");
+		driver.findElement(By.xpath("//*[@id=\"user_passwords\"]")).sendKeys("User1234");
+		System.out.println("Password Entered");
+		driver.findElement(By.xpath("//*[@id=\"user_confirm_password\"]")).sendKeys("User1234");
+		System.out.println("Password ReEntered");
+		driver.findElement(By.xpath("//*[@id=\"terms_conditions\"]")).click();
+		System.out.println("Terms of Service clicked");
+
+		driver.findElement(By.xpath("//*[@id=\"frmUserRegister\"]/input")).click(); 
+		System.out.println("Sign up Clicked");
+		driver.findElement(By.xpath("//*[@id=\"menu\"]/li[4]/ul/li/a/div")).click();
+		driver.findElement(By.xpath("//*[@id=\"menu\"]/li[4]/ul/li/ul/li[5]/a")).click();
+		System.out.println("logged out");
+	}
+
+	@Test(priority=2)
+	public void Userlogin() {
+		driver.findElement(By.xpath("//*[@id=\"user_email\"]")).sendKeys("user@13iscripts.com");
+		driver.findElement(By.xpath("//*[@id=\"frmLogin\"]/div/div[2]/input")).sendKeys("User1234");
+		driver.findElement(By.xpath("	//*[@id=\"frmLogin\"]/div/div[3]/input")).click();
+		System.out.println("Return user loggen in");
+	}
+	@Test(priority=3)
+
+	public void UserFirstPost() {
+		driver.findElement(By.xpath("//*[@id=\"logoform\"]/div/div[1]/div")).sendKeys("Hello This is My First Post");
+		driver.findElement(By.xpath("//*[@id=\"logoform\"]/div/div[2]/div[3]/input[2]")).click();
+	}
+
+	@AfterTest
+	public void terminateBrowser(){
+		driver.close();
+		//driver.quit();
+		System.out.println("Browser Closed");
+	}
+}
+
+
+
+
+
+/**
+public class Maven {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://demo.guru99.com/test/newtours/");
+		System.out.print("Browser opened");
+		driver.quit();
+		System.out.print("Browser Closed");
+
+}
+}
+
+@BeforeMethod
+public void Setup(){
+
+	driver.manage().window().maximize();
+	driver.manage().deleteAllCookies();
+	driver.manage().timeouts().pageLoadTimeout(20,TimeUnit.SECONDS);
+	driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+
+
+
+
+
+
+
+ */
+
